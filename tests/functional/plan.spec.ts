045956cf6d72bd9d75ab3cf8a84f845fdee765fa
Harrison Travers <travershh@gmail.com>
2024-01-13 21:48:27 +1100

    Added test for plan version creation when plan is created

--- tests/functional/plan.spec.ts
+++ tests/functional/plan.spec.ts
@@ -1,6 +1,7 @@
 import Database from "@ioc:Adonis/Lucid/Database";
 import { test } from "@japa/runner";
 import { UserFactory } from "Database/factories";
+import PlanVersion from "App/Models/PlanVersion";
 
 test.group("Plan", (group) => {
   group.each.setup(async () => {
@@ -8,7 +9,10 @@ test.group("Plan", (group) => {
     return () => Database.rollbackGlobalTransaction();
   });
 
-  test("create a plan", async ({ client, assert }) => {
+  test("create a plan, which also creates a plan version", async ({
+    client,
+    assert,
+  }) => {
     const user = await UserFactory.create();
     const response = await client
       .post("/plans")
@@ -17,10 +21,18 @@ test.group("Plan", (group) => {
       })
       .loginAs(user);
 
-    console.log("RESPONSE BODY: ", response.body());
-
     assert.exists(response.body().id);
     assert.equal(response.body().user_id, user.id);
     assert.equal(response.body().name, "Test Plan");
+
+    // Get all
+    const planVersion = await PlanVersion.query().where(
+      "plan_id",
+      response.body().id
+    );
+
+    assert.equal(planVersion.length, 1);
+    assert.equal(planVersion[0].planId, response.body().id);
+    assert.equal(planVersion[0].isActive, true);
   });
 });
